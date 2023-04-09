c7115b5fcdaa016c939322c5ce9f46fcda1fada4
akorede <emiolasaheed1@gmail.com>
2023-04-09 23:17:52 +0100

    Change background source

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,7 +3,7 @@ import SingleBar from './SingleBar'
 
 const SideBar = () => {
   return (
-    <div className="bg-mobile md:bg-desktop flex justify-center md:justify-start md:flex-col gap-8 p-4 md:col-span-1 md:h-screen h-36 bg-no-repeat bg-cover bg-center">
+    <div className="bg-mobile md:bg-[url(../../src/images/bg-sidebar-desktop.svg)] flex justify-center md:justify-start md:flex-col gap-8 p-4 md:col-span-1 md:h-screen h-36 bg-no-repeat bg-cover bg-center">
       <SingleBar step={1} text='YOUR INFO'/>
       <SingleBar step={2} text='SELECT PLAN'/>
       <SingleBar step={3} text='ADD-ONS'/>
